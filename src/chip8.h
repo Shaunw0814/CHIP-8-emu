667cf26a5d56ee206c2ca1028ac339195e3b46a3
shaunw0814 <s.wang0814@gmail.com>
2022-09-02 18:45:37 -0700

    VSCode, working with ImGUI sdl_opengl3

---
+++ src/chip8.h
@@ -0,0 +1,17 @@
+
+
+class chip8{
+    public:
+
+    //variables
+    unsigned short opcode;
+    unsigned char memory[4096];
+    unsigned char reg[16];
+
+    unsigned short pc;
+    unsigned short I;
+
+    //functions
+    
+
+};
